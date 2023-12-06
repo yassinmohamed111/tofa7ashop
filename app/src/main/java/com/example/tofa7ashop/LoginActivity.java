3bc8263700f81f27b87d80aebcd8cad0fb1c04cb
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2023-12-06 02:47:07 +0200

    3rd commit

--- app/src/main/java/com/example/tofa7ashop/LoginActivity.java
+++ app/src/main/java/com/example/tofa7ashop/LoginActivity.java
@@ -1,14 +1,130 @@
 package com.example.tofa7ashop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText editTextemail , editTextPassword ;
+    Button loginbtn ;
+    TextView changepassword  , registernow ;
+    FirebaseAuth mAuth ;
+    CheckBox checkBox ;
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Get the checkbox reference
+        CheckBox checkBox = findViewById(R.id.rememberMeCheckBox);
+
+        // Check if the "Remember Me" checkbox is checked
+        if (checkBox.isChecked()) {
+            // If checked, proceed to check if the user is already authenticated
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser != null) {
+                // User is signed in, proceed to main activity
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+        }
+        // If the checkbox is not checked, the normal login flow continues
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextemail = findViewById(R.id.logEmail);
+        editTextPassword = findViewById(R.id.logPassword);
+        loginbtn = findViewById(R.id.loginButton);
+        changepassword = findViewById(R.id.forgotpassword);
+        registernow = findViewById(R.id.registerTextView);
+        mAuth = FirebaseAuth.getInstance();
+
+        changepassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext() , ForgetPasswordActivity.class) ;
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registernow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext() , RegisterActivity.class) ;
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  email , password  ;
+                password  = String.valueOf(editTextPassword.getText());
+                email  = String.valueOf(editTextemail.getText());
+                if(TextUtils.isEmpty(email) ){
+                    Toast.makeText(LoginActivity.this , "Enter Email please" ,Toast.LENGTH_SHORT).show() ;
+                    return ;
+                }
+                if(TextUtils.isEmpty(password) ){
+                    Toast.makeText(LoginActivity.this , "Enter password please" ,Toast.LENGTH_SHORT).show() ;
+                    return ;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(LoginActivity.this, "LOGIN successful.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(getApplicationContext() , MainActivity.class) ;
+                                    startActivity(intent);
+                                    finish();
+                                    
+                                    
+                                    
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/tofa7ashop/RegisterActivity.java
+++ app/src/main/java/com/example/tofa7ashop/RegisterActivity.java
@@ -1,14 +1,106 @@
 package com.example.tofa7ashop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText editTextemail , editTextPassword  , editTextphone , editTextname , editTextsec ;
+    Button regBtn ;
+    TextView textView ;
+    FirebaseAuth mAuth ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editTextemail =  findViewById(R.id.regEmail) ;
+        editTextPassword = findViewById(R.id.regPassword) ;
+        editTextname = findViewById(R.id.regName) ;
+        editTextphone = findViewById(R.id.regPhone) ;
+        regBtn = findViewById(R.id.buttonRegister);
+        mAuth = FirebaseAuth.getInstance();
+        editTextsec = findViewById(R.id.regSec);
+        textView = findViewById(R.id.textViewLogin);
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext() , LoginActivity.class) ;
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name  , email , password , phone , sec ;
+                name  = String.valueOf(editTextname.getText());
+                email  = String.valueOf(editTextemail.getText());
+                password  = String.valueOf(editTextPassword.getText());
+                phone  = String.valueOf(editTextphone.getText());
+                sec  = String.valueOf(editTextsec.getText());
+
+                if(TextUtils.isEmpty(email) ){
+                    Toast.makeText(RegisterActivity.this , "Enter Email please" ,Toast.LENGTH_SHORT).show() ;
+                    return ;
+                }
+                if(TextUtils.isEmpty(password) ){
+                    Toast.makeText(RegisterActivity.this , "Enter password please" ,Toast.LENGTH_SHORT).show() ;
+                    return ;
+                }
+                if(TextUtils.isEmpty(phone) ){
+                    Toast.makeText(RegisterActivity.this , "Enter password please" ,Toast.LENGTH_SHORT).show() ;
+                    return ;
+                }
+                if(TextUtils.isEmpty(name) ){
+                    Toast.makeText(RegisterActivity.this , "Enter name please" ,Toast.LENGTH_SHORT).show() ;
+                    return ;
+                }
+                if(TextUtils.isEmpty(sec)  ){
+                    Toast.makeText(RegisterActivity.this , "Enter security question please" ,Toast.LENGTH_SHORT).show() ;
+                    return ;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    Toast.makeText(RegisterActivity.this, "account created.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
