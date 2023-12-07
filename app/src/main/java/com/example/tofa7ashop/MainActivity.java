6cf9f6bf956420a8809b2100c927e4f35ebf6a34
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2023-12-07 16:16:52 +0200

    updates

--- app/src/main/java/com/example/tofa7ashop/MainActivity.java
+++ app/src/main/java/com/example/tofa7ashop/MainActivity.java
@@ -5,15 +5,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView username ;
+    FirebaseAuth mAuth ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
         Button logoutButton = findViewById(R.id.logout);
+
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            // Name, email address
+            String email = user.getEmail();
+
+            username = findViewById(R.id.userName);
+            username.setText("Email: "+ email );
+        }
+        else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
         // For instance, if you want to navigate back to the login screen
         Intent intent = new Intent(this, LoginActivity.class);
+        FirebaseAuth.getInstance().signOut();
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
